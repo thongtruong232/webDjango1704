2df17df7b1bcaeaf18bec2dc1cabe02ea2c653d4
thongtruong232 <thongtruong232@gmail.com>
2025-04-23 14:19:11 +0700

    Update

--- WebDjango/settings.py
+++ WebDjango/settings.py
@@ -186,7 +186,7 @@ USE_TZ = True
 
 # Redis Configuration
 REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = os.getenv('REDIS_PORT', '6380')
+REDIS_PORT = os.getenv('REDIS_PORT', '6379')
 
 CACHES = {
     'default': {
