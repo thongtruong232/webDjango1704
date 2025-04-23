ce2bdf22aea9f20033b6fa655776986a9aa808eb
thongtruong232 <thongtruong232@gmail.com>
2025-04-23 10:42:03 +0700

    Update port redis to 6380

--- WebDjango/settings.py
+++ WebDjango/settings.py
@@ -187,7 +187,7 @@ USE_TZ = True
 
 # Redis Configuration
 REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = os.getenv('REDIS_PORT', '6379')
+REDIS_PORT = os.getenv('REDIS_PORT', '6380')
 
 CACHES = {
     'default': {
